e7b2e575ede7b26a9fb46419c4d59126876695d4
Sai S <saisank29@gmail.com>
2025-09-25 20:08:44 -0500

    second com

--- build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
+++ build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Programming Training Week 1";
+  public static final String MAVEN_NAME = "Programming Test 1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2025-09-25 20:38:40 EDT";
-  public static final long BUILD_UNIX_TIME = 1758847120245L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "b816e29d41d885c30d5de774b7fe139b1124cf30";
+  public static final String GIT_DATE = "2025-09-25 21:00:14 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-09-25 21:04:27 EDT";
+  public static final long BUILD_UNIX_TIME = 1758848667116L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
